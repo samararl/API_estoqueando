7bf5eaa81b6da0dde4877bee3a4f0c9d42c6f0fc
Samara Lipolis <samararochalipolis@gmail.com>
2018-08-28 19:37:09 -0300

    Alterando conexão

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,10 @@ const logger = require('winston');
 
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'estoqueando',
-  password: 'admin',
-  port: 5432,
+   url: 'postgres://fexfwmiddikrel:3d753ae8cb5d2457007ab801c40419832f635b9f3e51f5dcdd024d5216ad0dea@ec2-54-235-160-57.compute-1.amazonaws.com:5432/d9tf0el90fe65i',
+  extra: {
+    ssl: true,
+  },
 });
 logger.info('Pool iniciado');
 
