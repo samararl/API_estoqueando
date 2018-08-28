78ec06ff5ab2a54e916b0ede0536e336a93f5bfd
Samara Lipolis <samararochalipolis@gmail.com>
2018-08-28 20:55:51 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -3,14 +3,12 @@ const logger = require('winston');
 
 
 const pool = new Pool({
- user: 'samara',
-  host: 'estoqueando.cwldu7bor6j1.us-east-2.rds.amazonaws.com',
-  database: 'estoqueando',
-  password: 'estoqueando2018',
+  url:'postgres://samara:estoqueando2018@estoqueando.cwldu7bor6j1.us-east-2.rds.amazonaws.com/estoqueando?sslmode=verify-full&sslrootcert=config/rds-combined-ca-bundle.pem',
   port: 5432,
   ssl: true
 });
 logger.info('Pool iniciado');
+
 
 
 pool.on('release', () => console.log('pool => conexão retornada'));
