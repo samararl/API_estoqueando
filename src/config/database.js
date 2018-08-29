ca05d207c59f6eb3bba07eaea431cc65e2ebb6fc
Samara Lipolis <samararochalipolis@gmail.com>
2018-08-28 23:01:42 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -3,14 +3,8 @@ const logger = require('winston');
 
 
 const pool = new Pool({
-  // user: 'samara',
-  // host: 'estoqueando.cwldu7bor6j1.us-east-2.rds.amazonaws.com',
-  // database: 'estoqueando',
-  // password: 'estoqueando2018',
-  // url: 'postgres://daqscstutbfqso:859804edd0f85683982ab6833ad7665206522c8b9c247d09518c21f04ae3a8b5@ec2-50-17-194-129.compute-1.amazonaws.com:5432/dbjou3r9pqfb80',
-  url: process.env.POSTGRES_DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
-  // port: 5432,
 });
 logger.info('Pool iniciado');
 
