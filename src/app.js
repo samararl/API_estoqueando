46017cf1eea8fb4112e35582516f5f5a3d11a794
Samara Lipolis <samararochalipolis@gmail.com>
2018-08-28 21:59:27 -0300

    .

--- src/app.js
+++ src/app.js
@@ -15,9 +15,64 @@ const app = express();
 
 logger.info('Aplicação iniciada');
 
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/')) {
+    next();
+    return;
+  }
+
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    logger.debug('entra timeout');
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  };
+
+  waitAndSend();
+  next();
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(connectionMiddleware(pool));
+
+app.use(extendTimeoutMiddleware);
 
 app.use('/', index);
 app.use('/users', usersRoute);
--- src/routes/consultantRoute.js
+++ src/routes/consultantRoute.js
@@ -7,4 +7,5 @@ router.get('/', consultantController.get);
 router.put('/:id', consultantController.put);
 router.delete('/:id', consultantController.delete);
 router.post('/add', consultantController.post);
+
 module.exports = router;
