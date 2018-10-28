6ebaec92527a4492a35770b7d31657208ded6130
LAPTOP-M0V6OETS\Paloma <pa_serra1@hotmail.com>
2018-10-28 17:33:03 -0300

    no message

---
+++ src/routes/purchaseOrderRoute.js
@@ -0,0 +1,209 @@
+const express = require('express');
+const purchaseOrderController = require('../controllers/purchaseOrderController');
+
+const router = express.Router();
+
+
+/**
+  * @swagger
+  * /purchaseorder/add:
+  *   post:
+  *     summary: Criação de Pedido
+  *     description:
+  *       "Cria um pedido com os dados principais"
+  *     tags:
+  *       - Pedido - Inserir
+  *     parameters:
+  *       - name: body
+  *         in: body
+  *         required: true
+  *         schema:
+  *           type: object
+  *           required:
+  *             - id_consultant
+  *             - id_client
+  *             - order_date
+  *             - total_price
+  *             - sales_date
+  *             - status
+  *             - authenticate
+  *           properties:
+  *             id_consultant:
+  *               type: integer
+  *             id_client:
+  *               type: integer
+  *             order_date:
+  *               type: date
+  *             total_price:
+  *               type: decimal
+  *             sales_date:
+  *               type: date
+  *             status:
+  *               type: string
+  *             authenticate:
+  *               type: string
+  *
+  *           example: {
+  *             "purchaseOrderData": {
+  *               "id_consultant": "1",
+  *               "id_client": "3",
+  *               "order_date": "10/09/2018",
+  *               "total_price": "40",
+  *               "sales_date": "12/09/2018",
+  *               "status": "Em separação",
+  *               "authenticate": "$someToken01020304"
+  *              }
+  *           }
+  *     responses:
+  *       200:
+  *         description: "Pedido criado com sucesso"
+  *         schema:
+  *           type: object
+  *           properties:
+  *             succes:
+  *               type: boolean
+  *             message:
+  *               type: string
+  *         examples:
+  *           application/json: {
+  *             "success": true,
+  *              "message": "Criado com sucesso"
+  *           }
+  *       500:
+  *         description: Pedido não pôde ser criado
+  */
+
+router.post('/add', purchaseOrderController.post);
+
+
+/**
+  * @swagger
+  * /purchaseorder:
+  *   get:
+  *     tags:
+  *       - Pedido - buscar
+  *     description: Busca todos os pedidos cadastrados
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: body
+  *         in: body
+  *         required: true
+  *         schema:
+  *           type: object
+  *           required:
+  *             - authenticate
+  *           properties:
+  *             authenticate:
+  *               type: string
+  *     responses:
+  *       200:
+  *         description: Pedidos listados com sucesso
+*/
+
+router.get('/', purchaseOrderController.get);
+
+/**
+  * @swagger
+  * /purchaseorder/{id}:
+  *   put:
+  *     tags:
+  *       - Pedido - atualizar
+  *     description: Atualizar determinado pedido cadastrado por id
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: body
+  *         in: body
+  *         required: true
+  *         schema:
+  *           type: object
+  *           required:
+  *             - id_consultant
+  *             - id_client
+  *             - order_date
+  *             - total_price
+  *             - sales_date
+  *             - status
+  *             - authenticate
+  *           properties:
+  *             id_consultant:
+  *               type: integer
+  *             id_client:
+  *               type: integer
+  *             order_date:
+  *               type: date
+  *             total_price:
+  *               type: decimal
+  *             sales_date:
+  *               type: date
+  *             status:
+  *               type: string
+  *             authenticate:
+  *               type: string
+  *     responses:
+  *       200:
+  *         description: Pedido atualizado com sucesso
+*/
+
+router.put('/:id', purchaseOrderController.put);
+
+/**
+  * @swagger
+  * /purchaseorder/{id}:
+  *   put:
+  *     tags:
+  *       - Pedido - desativar
+  *     description: Desativar determinado pedido cadastrado por id
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: body
+  *         in: body
+  *         required: true
+  *         schema:
+  *           type: object
+  *           required:
+  *             - authenticate
+  *           properties:
+  *             authenticate:
+  *               type: string
+  *     responses:
+  *       200:
+  *         description: Pedido desativado com sucesso
+*/
+
+router.put('/disable/:id', purchaseOrderController.disablePurchaseorder);
+
+
+/**
+  * @swagger
+  * /purchaseorder/{id}:
+  *   delete:
+  *     tags:
+  *       - Pedido - excluir
+  *     description: Excluir determinado pedido cadastrado por id
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: id
+  *         description: Id do pedido
+  *         required: true
+  *         type: integer
+    *       - name: body
+  *         in: body
+  *         required: true
+  *         schema:
+  *           type: object
+  *           required:
+  *             - authenticate
+  *           properties:
+  *             authenticate:
+  *               type: string
+  *     responses:
+  *       200:
+  *         description: Pedido excluído com sucesso
+  */
+router.delete('/:id', purchaseOrderController.delete);
+
+module.exports = router;
